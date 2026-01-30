9aecc5e651cfef6910391f67a77542307d4f8d25
lagartixa <anacristalcardosooliveira1@gmail.com>
2026-01-30 16:59:45 -0300

    Inputs

---
+++ novo-projeto-de-jogo/Cenas/Scripts/laser.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var speed := 500.0
+
+var movement_vector := Vector2(0, -1)
+
+
+
+func _physics_process(delta):
+	global_position += movement_vector.rotated(rotation) * speed * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
